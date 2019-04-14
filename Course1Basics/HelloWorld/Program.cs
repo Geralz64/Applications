005ed49bdd30cada06ba890f8e350829df3f4d34
Geralz64 <gdsizquierdo@gmail.com>
2019-04-14 09:55:29 -0400

    HelloWorldApp
    First application since I deleted the other one

---
+++ Course1Basics/HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write down your name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello there " + name);
+
+            Console.WriteLine("How many hours of sleep did you get today?");
+
+            int hours = Convert.ToInt32(Console.ReadLine());
+                
+
+            if(hours > 8)
+            {
+
+                Console.WriteLine("You are well rested");
+
+
+            }
+            else
+            {
+                Console.WriteLine("You need more rest");
+
+            }
+
+        }
+    }
+}
