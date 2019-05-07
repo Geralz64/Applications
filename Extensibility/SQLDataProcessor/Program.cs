2ece28974109f6be2571ff8e7dae17c3a216019b
Geralz64 <gdsizquierdo@gmail.com>
2019-05-06 21:03:53 -0400

    Joining LINQ
    Made some practice queries by using linq

--- Extensibility/SQLDataProcessor/Program.cs
+++ Extensibility/SQLDataProcessor/Program.cs
@@ -16,6 +16,7 @@ namespace SQLDataProcessor
              Query format practice and lambda expression format
              */
 
+            LeftJoinQueries();
 
             SingleTableQueries();
 
@@ -23,6 +24,88 @@ namespace SQLDataProcessor
 
             TableJoinQueries();
 
+            LeftJoinQueries();
+
+            InnerJoinLambdaExpression();
+
+
+
+
+        }
+
+        private static void InnerJoinLambdaExpression()
+        {
+
+
+            var db = new Repository.SQL.AdventureWorks2012Entities();
+            //BEFORE
+            var addresses = (from address in db.Addresses
+                             join province in db.StateProvinces
+                             on address.StateProvinceID equals province.StateProvinceID
+                             select address);
+            //AFTER
+            var addressesLambda = db.Addresses.Join(
+                                             db.StateProvinces,
+                                             a => a.StateProvinceID,
+                                             m => m.StateProvinceID,
+                                             (a, m) => new
+                                             {
+
+                                                 a.StateProvince,
+                                                 m.StateProvinceID
+
+                                             })
+                                             .OrderBy(a => a.StateProvince);
+            //BEFORE
+            var persons = (from person in db.People
+                           join emails in db.EmailAddresses
+                           //on new { person.BusinessEntityID } equals new { emails.BusinessEntityID } into A
+                           on person.BusinessEntityID equals emails.BusinessEntityID into A
+                           from emails in A.DefaultIfEmpty()
+                           where emails == null
+                           select person).ToList<Person>();
+
+
+            //AFTER
+            var personsLambda = db.People.Join(
+                                                db.EmailAddresses,
+                                                p => p.BusinessEntityID,
+                                                e => e.BusinessEntityID,
+                                                (p, e) => new
+                                                        {
+                                                            p.BusinessEntity,
+                                                            p.Demographics,
+                                                            e.EmailAddress1,
+                                                            e.ModifiedDate
+                                                        });
+
+            //AFTER V2
+            var personsLambda2 = db.People.Join(
+                                                db.EmailAddresses,
+                                                p => p.BusinessEntityID,
+                                                e => e.BusinessEntityID,
+                                                (p, e) => new
+                                                {p,e }
+                                                );
+        }
+
+        private static void LeftJoinQueries()
+        {
+
+
+            using (var db = new Repository.SQL.AdventureWorks2012Entities())
+            {
+
+
+                var results = (from person in db.People
+                               join emails in db.EmailAddresses
+                               //on new { person.BusinessEntityID } equals new { emails.BusinessEntityID } into A
+                               on person.BusinessEntityID equals emails.BusinessEntityID into A
+                               from emails in A.DefaultIfEmpty()
+                               where emails == null
+                               select person).ToList<Person>();
+
+            }
 
         }
 
@@ -32,29 +115,29 @@ namespace SQLDataProcessor
             var db = new Repository.SQL.AdventureWorks2012Entities();
 
             var addresses = (from address in db.Addresses
-                          join province in db.StateProvinces
-                          on address.StateProvinceID equals province.StateProvinceID
-                          select address).ToList<Address>();
+                             join province in db.StateProvinces
+                             on address.StateProvinceID equals province.StateProvinceID
+                             select address).ToList<Address>();
 
 
             var providences = (from address in db.Addresses
-                           join province in db.StateProvinces
-                           on address.StateProvinceID.ToString() equals province.StateProvinceID.ToString()
-                           select province).Distinct().ToList<StateProvince>();
+                               join province in db.StateProvinces
+                               on address.StateProvinceID.ToString() equals province.StateProvinceID.ToString()
+                               select province).Distinct().ToList<StateProvince>();
 
 
             var addresses2 = (from address in db.Addresses
-                             join province in db.StateProvinces
-                             on address.StateProvinceID equals province.StateProvinceID
-                             where address.AddressID == 1 && address.City == "Bothell" && province.StateProvinceID == 79
+                              join province in db.StateProvinces
+                              on address.StateProvinceID equals province.StateProvinceID
+                              where address.AddressID == 1 && address.City == "Bothell" && province.StateProvinceID == 79
                               select address).ToList<Address>();
 
 
             var providences2 = (from address in db.Addresses
-                               join province in db.StateProvinces
-                               on address.StateProvinceID.ToString() equals province.StateProvinceID.ToString()
-                               where province.CountryRegionCode == "FR"
-                               select province).ToList<StateProvince>();
+                                join province in db.StateProvinces
+                                on address.StateProvinceID.ToString() equals province.StateProvinceID.ToString()
+                                where province.CountryRegionCode == "FR"
+                                select province).ToList<StateProvince>();
 
 
         }
