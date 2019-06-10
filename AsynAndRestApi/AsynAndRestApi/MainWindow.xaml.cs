e9802dcf3978e78e27e835174b848eb86d6064f2
Geralz64 <gdsizquierdo@gmail.com>
2019-06-09 21:52:01 -0400

    Cancellation token
    Got the cancellation token working

--- AsynAndRestApi/AsynAndRestApi/MainWindow.xaml.cs
+++ AsynAndRestApi/AsynAndRestApi/MainWindow.xaml.cs
@@ -8,54 +8,100 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Tweetinvi;
 using Tweetinvi.Models;
+using System.Linq;
+using System.Windows.Media;
 
 namespace AsynAndRestApi
 {
-    
+
     public partial class MainWindow : Window
     {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
-        public static string imageURl;
-        public static IEnumerable<ITweet> tweets;
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         public async void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            await LongTakingTask();
+            try
+            {
+                var userInfo = GetTwitterInfo();
 
-            Image img = new Image();
-            imgUser.Source = new BitmapImage(new Uri(imageURl));
+                var tweets = await LongTakingProcess(userInfo, cancellationTokenSource.Token);
 
-            dgInfo.ItemsSource = tweets;
+                var imageURl = userInfo.ProfileImageUrl;
+
+                Image img = new Image();
+                imgUser.Source = new BitmapImage(new Uri(imageURl));
+
+                dgInfo.ItemsSource = tweets;
+
+            }
+            catch (OperationCanceledException ex)
+
+            {
+                textBox.Text = "Operation was cancelled by the user";
+                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+        }
+
+        public static Task<List<ITweet>> LongTakingProcess(IAuthenticatedUser userInfo, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+                Task<List<ITweet>> task = null;
+
+                var tweets = Timeline.GetUserTimeline(userInfo.UserIdentifier, 200);
+
+                var newTweets = new List<ITweet>();
+
+                task = Task.Run(() =>
+                {
+                    foreach (var tweet in tweets)
+                    {
+                        newTweets.Add(tweet);
+
+                        cancellationToken.ThrowIfCancellationRequested();
+                        Thread.Sleep(5000);
+
+                    }
+
+                    newTweets = newTweets.Where(t => t.Text.Contains("#100DaysOfCode")).OrderBy(t => t.Id).ToList<ITweet>();
+
+                    return newTweets;
+
+                });
+
+                return task;
+            }
+            catch(OperationCanceledException ex)
+            {
+
+                throw;
+            }
         }
 
 
-
-        public static Task LongTakingTask()
+        public static IAuthenticatedUser GetTwitterInfo()
         {
-            return Task.Run(() =>
-            {
-                #region keys
-                string consumerKey = "yfmUxeF6eGHSkPqwJh3Th1AuN";
-                string consumerSecret = "5IU13cfNx983O4DQ9caR45zmll5a9BKLSt1jKKcodV6AAcNXz0";
-                string userAccessToken = "217835459-SyE1LoJ70JtLfCZwx9hFwbhernmMjK1FJz6CvPsF";
-                string userAccessSecret = "GP64AUOiLNgUj4Z6JSDHyPLv5JjfNT1potfWqHvzjSDQo";
-                #endregion
+            var tokenSource2 = new CancellationTokenSource();
+            #region keys
+            string consumerKey = "yfmUxeF6eGHSkPqwJh3Th1AuN";
+            string consumerSecret = "5IU13cfNx983O4DQ9caR45zmll5a9BKLSt1jKKcodV6AAcNXz0";
+            string userAccessToken = "217835459-SyE1LoJ70JtLfCZwx9hFwbhernmMjK1FJz6CvPsF";
+            string userAccessSecret = "GP64AUOiLNgUj4Z6JSDHyPLv5JjfNT1potfWqHvzjSDQo";
+            #endregion
 
-                Auth.SetUserCredentials(consumerKey, consumerSecret, userAccessToken, userAccessSecret);
-                var userInfo = User.GetAuthenticatedUser();
+            Auth.SetUserCredentials(consumerKey, consumerSecret, userAccessToken, userAccessSecret);
+            var userInfo = User.GetAuthenticatedUser();
+            return userInfo;
 
-                imageURl = userInfo.ProfileImageUrl;
-
-                tweets = Timeline.GetUserTimeline(userInfo.UserIdentifier, 200);
-
-                Thread.Sleep(10000);
-
-            });
         }
 
         private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -67,7 +113,7 @@ namespace AsynAndRestApi
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Clear_Click_1(object sender, RoutedEventArgs e)
         {
 
             imgUser.Source = null;
@@ -75,5 +121,14 @@ namespace AsynAndRestApi
             textBox.Text = string.Empty;
 
         }
+
+        public void Cancel_Click_1(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+
+        }
+
+
+
     }
 }
