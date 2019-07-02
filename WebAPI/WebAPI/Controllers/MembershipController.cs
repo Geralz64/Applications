ca51801d39ddc89c953d29d54ca86f94f26cf62b
Geralz64 <gdsizquierdo@gmail.com>
2019-07-01 20:57:15 -0400

    New API solution
    Addednew solution

---
+++ WebAPI/WebAPI/Controllers/MembershipController.cs
@@ -0,0 +1,52 @@
+﻿using Repository.TestData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("api/membership")]
+    public class MembershipController : ApiController
+    {
+
+        [Route()]
+        public async Task<IHttpActionResult> Get()
+        {
+
+            try
+            {
+                var result = await MembershipTestData.TestDataAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                InternalServerError(ex);
+            }
+
+            return null;
+
+        }
+
+        [Route("{memberID}")]
+        public async Task<IHttpActionResult> Get(string memberID)
+        {
+
+            try
+            {
+                var result = await MembershipTestData.TestDataAsync(memberID);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                InternalServerError(ex);
+            }
+
+            return null;
+        }
+
+
+    }
+}
